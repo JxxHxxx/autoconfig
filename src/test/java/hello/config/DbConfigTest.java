e2018a21994d7cf2d58b74cd428259be7c3c91cf
JxxHxxx <leesin5498@naver.com>
2023-09-18 22:31:29 +0900

    Initial commit

---
+++ src/test/java/hello/config/DbConfigTest.java
@@ -0,0 +1,31 @@
+package hello.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionManager;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@SpringBootTest
+class DbConfigTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    TransactionManager transactionManager;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    void checkBean() {
+        log.info("dataSource = {}", dataSource);
+        log.info("transactionManager = {}", transactionManager);
+        log.info("jdbcTemplate = {}", jdbcTemplate);
+    }
+}
